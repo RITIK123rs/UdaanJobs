fe13dac06a9e35a931c0c3e620c8d7ab054b8ab8
Ritik Singh <sritiksingh246@gmail.com>
2026-01-22 09:01:32 +0530

    add

--- server/routes/loginPage.js
+++ server/routes/loginPage.js
@@ -11,106 +11,6 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-mailData = {
-        from: `UdaanJobs <udaanjobs.rs@gmail.com>`,
-        to: "dk7025146@gmail.com",
-        subject: "Email Verification OTP",
-        html: `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OTP Verification</title>
-</head>
-
-<body>
-    <h1 style="padding: 20px; text-align: center; background:#0a66c2; color:#ffffff;">UdaanJobs</h1>
-    <div style="padding: 20px 10px;">
-
-        <p style="font-size:16px;">Dear User,</p>
-
-        <p style="font-size:15px;">
-            Thank you for signing up with <strong>UdaanJobs</strong>.
-            To complete your registration, please use the One-Time Password (OTP) below:
-        </p>
-
-        <div style="margin: 35px 0; text-align:center;">
-            <span style="color: #0a66c2; font-weight: bold; background-color: #0a66c251; padding: 10px 20px; border-radius: 5px; font-size: 25px; letter-spacing: 7px;" >
-                1234
-            </span>
-        </div>
-
-        <p style="font-size:14px; color: #484545;">
-            This OTP is valid for <strong>5 minutes</strong> only.
-            Please do not share this OTP with anyone for security reasons.<br><br><br>
-            If you did not initiate this request, please ignore this email.
-        </p>
-
-    </div>
-</body>
-
-</html>
-`,}
-    const firstName = "John";
-    let info = await transporter.sendMail(mailData );
-
-    console.log("Message sent: %s", info.messageId);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const transporter = nodemailer.createTransport({
   // service: "gmail",
   // auth: {
@@ -125,6 +25,11 @@ const transporter = nodemailer.createTransport({
     user: process.env.Smtp_user,
     pass: process.env.Smtp_pass,
   },
+});
+
+transporter.verify((err) => {
+  if (err) console.error("SMTP error:", err);
+  else console.log("Brevo SMTP ready");
 });
 
 const otpStore = {};
