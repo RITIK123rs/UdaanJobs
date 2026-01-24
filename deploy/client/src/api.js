2e9a3ea90ec02ff8fdf30f04e09b42c0e6b3ac64
Ritik <your_email@example.com>
2026-01-24 21:35:44 +0530

    add

--- deploy/client/src/api.js
+++ deploy/client/src/api.js
@@ -1,6 +1,6 @@
 
 
-// export const API_URL = `https://udaanjobsserver.vercel.app`;
+export const API_URL = `https://udaanjobsserver.vercel.app`;
 
-export const API_URL = `http://localhost:3200`;
+// export const API_URL = `http://localhost:3200`;
 
